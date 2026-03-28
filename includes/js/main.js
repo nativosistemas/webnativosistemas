db722658c18fb020902c303cd739a3c4a8bfa78a
nativosistemas <nativosistemas@users.noreply.github.com>
2026-03-28 00:44:56 +0000

    deploy: a30c25473a64067e5693bf43bbc27e589c5d626e

---
+++ includes/js/main.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    //document.getElementById("year").innerHTML = new Date().getFullYear() ;
+    $("#menu-close").click(function (e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+    $("#menu-toggle").click(function (e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+
+});
